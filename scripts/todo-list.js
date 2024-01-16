58b00a4ecc66da56dae52b6940f7eb9ed3865f18
Nrosta <153558060+Astornek@users.noreply.github.com>
2024-01-16 16:44:52 +0100

    Update todo-list.js

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,9 +1,5 @@
-const todoList = [
-  {
-    name: "make dinner",
-    dueDate: "2024-01-13",
-  },
-];
+// Sprawdź, czy istnieją dane w local storage, jeśli nie, użyj pustej tablicy
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 renderTodoList();
 
@@ -12,13 +8,12 @@ function renderTodoList() {
   todoList.forEach(function (todoObject, index) {
     const { name, dueDate } = todoObject;
     const html = `
-
      <div> ${name}</div><div> ${dueDate} </div>
       <button onclick="
         todoList.splice(${index}, 1);
         renderTodoList();
+        saveToLocalStorage();
       " class="delete-todo-button">Delete</button> 
-
     `;
     todoListHTML += html;
   });
@@ -33,11 +28,17 @@ function addTodo() {
   const dueDate = dateInputElement.value;
 
   todoList.push({
-    //name: name,
-    //dueDate: dueDate,
     name,
     dueDate,
   });
+
   inputElement.value = "";
+  dateInputElement.value = "";
+
   renderTodoList();
+  saveToLocalStorage();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
